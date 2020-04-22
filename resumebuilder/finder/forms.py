b4c5db66a8a05e0505f5852b53599bd6c92a9fb5
Milad Hatami <miladhzz@gmail.com>
2020-04-22 22:26:07 +0430

    add novalidate

--- resumebuilder/finder/forms.py
+++ resumebuilder/finder/forms.py
@@ -5,7 +5,7 @@ attr = {'class': 'form-control'}
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(widget=forms.TextInput(attrs=attr))
+    name = forms.CharField(widget=forms.TextInput(attrs=attr), max_length=5)
     family = forms.CharField(widget=forms.TextInput(attrs=attr))
-    email = forms.EmailField(widget=forms.TextInput(attrs=attr))
+    email = forms.EmailField(widget=forms.EmailInput(attrs=attr))
     message = forms.CharField(widget=forms.Textarea(attrs=attr))
