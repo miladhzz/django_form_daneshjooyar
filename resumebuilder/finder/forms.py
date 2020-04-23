3af0704e5c6f8391eb1737aedb0e10a9ada76325
Milad Hatami <miladhzz@gmail.com>
2020-04-23 16:05:09 +0430

    add custom validation

--- resumebuilder/finder/forms.py
+++ resumebuilder/finder/forms.py
@@ -5,7 +5,13 @@ attr = {'class': 'form-control'}
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(widget=forms.TextInput(attrs=attr), max_length=5)
+    name = forms.CharField(widget=forms.TextInput(attrs=attr))
     family = forms.CharField(widget=forms.TextInput(attrs=attr))
     email = forms.EmailField(widget=forms.EmailInput(attrs=attr))
     message = forms.CharField(widget=forms.Textarea(attrs=attr))
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if len(name) > 10:
+            raise forms.ValidationError("The name is long.")
+        return name
