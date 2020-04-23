f278a0d6827d445f2e5146923cc3f98792c163b5
Milad Hatami <miladhzz@gmail.com>
2020-04-23 16:51:07 +0430

    override clean method

--- resumebuilder/finder/forms.py
+++ resumebuilder/finder/forms.py
@@ -1,17 +1,27 @@
 from django import forms
+from django.core import validators
 
 
 attr = {'class': 'form-control'}
 
 
+def check_name_length(value):
+    if len(value) > 10:
+        raise forms.ValidationError("The name is long.")
+
+
 class ContactForm(forms.Form):
-    name = forms.CharField(widget=forms.TextInput(attrs=attr))
+    name = forms.CharField(widget=forms.TextInput(attrs=attr),
+                           validators=[check_name_length])
     family = forms.CharField(widget=forms.TextInput(attrs=attr))
     email = forms.EmailField(widget=forms.EmailInput(attrs=attr))
+    retype_email = forms.EmailField(widget=forms.EmailInput(attrs=attr))
     message = forms.CharField(widget=forms.Textarea(attrs=attr))
 
-    def clean_name(self):
-        name = self.cleaned_data['name']
-        if len(name) > 10:
-            raise forms.ValidationError("The name is long.")
-        return name
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        retype_email = cleaned_data.get('retype_email')
+
+        if email != retype_email:
+            raise forms.ValidationError("email and retype email is not equal !")
