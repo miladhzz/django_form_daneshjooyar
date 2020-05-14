7c1705bc86d0c59134aef60dafc0aab4e7131847
Milad Hatami <miladhzz@gmail.com>
2020-05-14 12:55:27 +0430

    add resume generator

--- resumebuilder/generator/views.py
+++ resumebuilder/generator/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+from creator import models
 
 
+@login_required
 def generate_resume(request):
-    return render(request, 'generate_resume.html')
+    resume = get_object_or_404(models.Resume, user=request.user)
+    print(resume.id)
+    resume_skills = models.ResumeSkill.objects.filter(resume=resume)
+    resume_educations = models.ResumeEducation.objects.filter(resume=resume)
+    resume_experiences = models.ResumeExperience.objects.filter(resume=resume)
+    return render(request, 'generate_resume.html', {'resume': resume,
+                                                    'resume_skills': resume_skills,
+                                                    'resume_educations': resume_educations,
+                                                    'resume_experiences': resume_experiences})
 
